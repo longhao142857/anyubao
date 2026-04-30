98f8b96701de684dda80e626e3f0ecd2b2fd2f7f
longhao142857 <1073861760@qq.com>
2026-04-30 21:21:13 +0800

    ??Service Worker

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'anyubao-v1';
+const urlsToCache = [
+  './',
+  './暗语宝.html',
+  './manifest.json'
+];
+
+// 安装
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// 激活
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(names => {
+      return Promise.all(
+        names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// 请求拦截
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      if (res) return res;
+      return fetch(e.request).then(response => {
+        if (!response || response.status !== 200) return response;
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, clone);
+        });
+        return response;
+      }).catch(() => {
+        return new Response('离线状态');
+      });
+    })
+  );
+});
